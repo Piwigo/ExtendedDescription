5b80c0792433bdbc4412673ca1ec6b0eeb40cc1a
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-12-20 08:55:31 +0100

    Update he_IL, thanks to epifeny

---
+++ language/he_IL/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Carrousel'] = 'קרוסלה';
+$lang['Descriptions'] = 'תיאורים';
+$lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = 'התיאור המורחב הותקן בהצלחה. כעת ניתן להשתמש בכל התכונות שלו ברוב תיבות הטקסט של Piwigo.';
+$lang['Extended descriptions'] = 'תיאורים מורחבים';
+$lang['Hide elements'] = 'הסתר רכיבים';
+$lang['Insert a carousel'] = 'הוסף קרוסלה';
+$lang['Insert an album or a photo'] = 'הוסף אלבום או תמונה';
+$lang['Login block'] = 'בלוק התחברות';
+$lang['Login link & logged in block'] = 'קישור התחברות ובלוק מחובר';
+$lang['Multilingual'] = 'רב-שפתי';
+$lang['Multilingual descriptions'] = 'תיאורים רבי-שפה';
+$lang['Redirect elements'] = 'הפניית רכיבים';
+$lang['Redirects'] = 'הפניות';
+$lang['This category is redirected to %s'] = 'אלבום זה מופנה אל %s';
+$lang['Use Extended Description tags...'] = 'השתמש בתגי תיאור מורחב...';
