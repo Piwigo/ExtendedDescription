997b22a7075000561c7f4cce6ba4a20a139f8bae
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-09 08:24:35 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,22 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Carrousel'] = '캐러셀';
+$lang['Descriptions'] = '설명';
+$lang['Extended Description have been successfully installed. Now you can use all its features in most text boxes of Piwigo.'] = 'Extended Description 기능이 성공적으로 설치되었습니다. 이제 Piwigo의 대부분의 텍스트 상자에서 모든 기능을 사용할 수 있습니다.';
+$lang['Extended descriptions'] = '확장 설명';
+$lang['Hide elements'] = '요소 숨기기';
+$lang['Insert a carousel'] = '캐러셀 삽입';
+$lang['Insert an album or a photo'] = '앨범 또는 사진 삽입';
+$lang['Login block'] = '로그인 블록';
+$lang['Login link & logged in block'] = '로그인 링크 및 로그인 블록';
+$lang['Multilingual'] = '다국어';
+$lang['Multilingual descriptions'] = '다국어 설명';
+$lang['Redirect elements'] = '리디렉션 요소';
+$lang['Redirects'] = '리디렉션';
+$lang['This category is redirected to %s'] = '이 앨범은 %s로 리디렉션됩니다';
+$lang['Use Extended Description tags...'] = '확장 설명 태그 사용...';
